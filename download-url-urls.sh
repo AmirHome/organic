3d456b20b0693c9fa9f84aba9898be1819755d4f
AmirHome <amir.email@yahoo.com>
2024-04-18 17:11:05 +0300

    init

---
+++ download-url-urls.sh
@@ -0,0 +1,4 @@
+wget -P assets/images/backgrounds https://pixydrops.com/ogenix/main-html/assets/images/backgrounds/page-header-bg.jpg
+wget -P assets/images/shapes https://pixydrops.com/ogenix/main-html/assets/images/shapes/page-header-ripped-paper.png
+wget -P assets/images/backgrounds https://pixydrops.com/ogenix/main-html/assets/images/backgrounds/site-footer-bg-img.png
+wget -P assets/images/shapes https://pixydrops.com/ogenix/main-html/assets/images/shapes/site-footer-ripped-paper.png
